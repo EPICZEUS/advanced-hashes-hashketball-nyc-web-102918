ea62a53ae5f9c91bde84823b4e4f93f48a8f92d8
Sam B <s77broz@aol.com>
2018-10-13 05:18:04 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code here!
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -118,10 +120,12 @@ end
 
 def num_points_scored(player)
   game_hash.find do |key, val|
+    binding.pry
     val[:players].has_key?(player)
   end[:points]
 end
 
+num_points_scored("Jeff Adrien")
 
 
 
